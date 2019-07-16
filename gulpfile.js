1588a4f77eac0dbb1e89a2033e304de432911065
pawansa3 <pawansa3@noreply.github.com>
2019-07-16 10:00:42 +0530

    gulp basics & default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
 "use strict";
 
 var gulp = require("gulp");
+
+// adding gulp simple hello task
+gulp.task("hello", function() {
+  console.log("hi there!");
+});
+
+// adding gulp default task in parallel with devs task hello
+gulp.task(
+  "default",
+  gulp.parallel("hello", function() {
+    console.log("default task running!");
+  })
+);
